f3d119b89a62282561f64554eabd64914640ad10
Rubens Algeri <rubens_zys@hotmail.com>
2022-07-18 00:13:43 -0300

    feat: implemented signIn in the POST/signin route

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import * as authService from '../services/authService.js';
 
 export async function signIn(req:Request, res:Response) {
-    
+    const {email, password} = req.body;
+    const data={email,password}
+    const token = await authService.login(data);
+    res.status(200).send(token)
 }
 
 export async function signUp(req:Request, res:Response) {
@@ -10,5 +13,5 @@ export async function signUp(req:Request, res:Response) {
     const data={email,password}
     await authService.checkEmail(email);
     await authService.createUser(data);
-    res.sendStatus(201)
+    res.status(201)
 }
--- services/authService.ts
+++ services/authService.ts
@@ -1,13 +1,25 @@
 import * as authRepository from "../repositories/authRepository.js";
 import { User } from "@prisma/client";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
 
 export type CreateUserData = Omit<User,'id'>;
-
-export async function createUser(data:CreateUserData) {
-    await authRepository.createUser(data);
-}
 
 export async function checkEmail(email:string) {
     const check = await authRepository.checkEmail(email)
     if(check)throw{type:409,message:"Email is already used"}
+}
+
+export async function createUser(data:CreateUserData) {
+    data.password = bcrypt.hashSync(data.password, +process.env.HASH);
+    await authRepository.createUser(data);
+}
+
+export async function login(data: CreateUserData) {
+    const user = await authRepository.checkEmail(data.email)
+    if(!user)throw{type:404,message:"Email is not found"}
+
+    if (!bcrypt.compareSync(data.password, user.password)) throw{type:401,message:"Check your email/password"}
+    const token = jwt.sign(data.email, process.env.JWT_SECRET);
+    return token;
 }
